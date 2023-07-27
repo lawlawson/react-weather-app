6aed9cfeafa713b017a199aacb29b29267045d66
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-07-27 17:13:46 +0100

    styled current weather

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -4,10 +4,12 @@ const currentWeather = () => {
   return (
     <div className='weather'>
       <div className='top'>
-        <p className='city'>Accra</p>
-        <p className='weather-description'>Sunny</p>
+        <div>
+          <p className='city'>Accra</p>
+          <p className='weather-description'>Sunny</p>
+        </div>
+        <img className='weather-icon' src='icons/01d.png' alt='' />
       </div>
-      <img className='weather-icon' src='icons/01d.png' alt='' />
     </div>
   );
 };
