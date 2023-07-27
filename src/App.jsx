9ec36d4be77e00c4198ea07ce44697e3e874733d
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-07-28 00:15:33 +0100

    first fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Search from './components/Search/Search';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './components/api';
 
 function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(' ');
 
-    const currentWeatherFetch = fetch(`/weather?lat={lat}&lon={lon}&appid={API key}`)
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
   };
 
   return (
--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,6 @@
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '82778542a98457a3c39ca2ffe17ce0c9';
 
 export const geoApiOptions = {
   method: 'GET',
