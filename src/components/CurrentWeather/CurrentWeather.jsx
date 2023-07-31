e2b113490dd50f013ed02f8e30fa1f2bd0b205c8
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-07-31 18:53:20 +0100

    added remainding dynamic data

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -15,27 +15,29 @@ const currentWeather = ({ data }) => {
         />
       </div>
       <div className='bottom'>
-        <p className='temperature'>18°C</p>
+        <p className='temperature'>{Math.round(data.main.temp)}°C</p>
         <div className='details'>
           <div className='parameter-row'>
             <span className='parameter-label top'>Details</span>
           </div>
           <div className='parameter-row'>
             <span className='parameter-label'>Feels like</span>
-            <span className='parameter-value'>22°C</span>
+            <span className='parameter-value'>
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           <div className='parameter-row'>
             <span className='parameter-label'>Wind</span>
-            <span className='parameter-value'>2 m/s</span>
+            <span className='parameter-value'>{data.wind.speed}m/s</span>
           </div>
           <div className='parameter-row'>
             <span className='parameter-label'>Humidity</span>
-            <span className='parameter-value'>15%</span>
+            <span className='parameter-value'>{data.main.humidity}%</span>
           </div>
-          <div className='parameter-row'>
+          {/* <div className='parameter-row'>
             <span className='parameter-label'>Pressure</span>
             <span className='parameter-value'>15 hPa</span>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
