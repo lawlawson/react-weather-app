c20e8172d28957223995397c96918105a9551d2b
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-08-01 18:29:01 +0100

    added forecast info

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -6,7 +6,22 @@ import {
   AccordionItemPanel,
 } from 'react-accessible-accordion';
 
+const WEEK_DAYS = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
 const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
   return (
     <>
       <label className='title'>Daily</label>
@@ -21,6 +36,14 @@ const Forecast = ({ data }) => {
                     alt=''
                     className='icon-small'
                   />
+                  <label className='day'>{forecastDays[index]}</label>
+                  <label className='description'>
+                    {item.weather[0].description}
+                  </label>
+                  <label className='min-max'>
+                    {Math.round(item.main.temp_min)}°C /{' '}
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
