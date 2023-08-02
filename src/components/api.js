873833f7600f2ea3c2791f865bd353355aaab3bb
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-08-02 14:42:52 +0100

    removed info

--- src/components/api.js
+++ src/components/api.js
@@ -1,11 +1,11 @@
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = '82778542a98457a3c39ca2ffe17ce0c9';
+export const WEATHER_API_KEY = ''; //enter your own api key from OpenWeatherAPI
 
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '45e189e052mshddfc8e800264449p135f0fjsn79b4f9c599ff',
+    'X-RapidAPI-Key': '', //enter your own api key
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
